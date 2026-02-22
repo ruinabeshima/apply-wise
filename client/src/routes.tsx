421a48f1bd4eab1d6266bff131b60f1f92a0e41a
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-22 20:09:54 +0900

    feat: added protected route handler for /dashboard

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,13 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Home from "./components/home";
 import { SignIn, SignUp } from "@clerk/clerk-react";
+import ProtectedRoute from "./protected_routes";
 
 const routes = (
   <Routes>
     <Route path="/" element={<Home />}></Route>
     <Route path="/register" element={<SignUp signInUrl="/login" />}></Route>
     <Route path="/login" element={<SignIn signUpUrl="/register" />}></Route>
-    <Route path="/dashboard" element={<Dashboard />}></Route>
+    <Route
+      path="/dashboard"
+      element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      }
+    ></Route>
   </Routes>
 );
 
