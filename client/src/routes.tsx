6919820837d99982a9204fca8eab4e3d887111a9
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-22 18:31:28 +0900

    feat: added Clerk sign in and sign up components

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/login";
-import Register from "./components/register";
 import Dashboard from "./components/dashboard";
 import Home from "./components/home";
+import { SignIn, SignUp } from "@clerk/clerk-react";
 
 const routes = (
   <Routes>
     <Route path="/" element={<Home />}></Route>
-    <Route path="/register" element={<Register />}></Route>
-    <Route path="/login" element={<Login />}></Route>
+    <Route path="/register" element={<SignUp signInUrl="/login" />}></Route>
+    <Route path="/login" element={<SignIn signUpUrl="/register" />}></Route>
     <Route path="/dashboard" element={<Dashboard />}></Route>
   </Routes>
 );
