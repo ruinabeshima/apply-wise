61c004eaef6e997627a1a910dbad474989204b1c
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-22 19:59:27 +0900

    feat: added webhook handling for user.created, user.updated, and user.deleted from clerk

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { clerkMiddleware } = require("@clerk/express");
+const { prisma } = require("./lib/prisma");
+const { Webhook } = require("svix");
 
 require("dotenv").config();
 
@@ -11,12 +13,59 @@ app.use(
   }),
 );
 app.use(clerkMiddleware());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.post(
+  "/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // Verify the webhook
+    let evt;
+    try {
+      evt = wh.verify(req.body, {
+        "svix-id": req.headers["svix-id"],
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"],
+      });
+    } catch (error) {
+      return res.status(400).json({ error: "Invalid signature" });
+    }
+
+    // New user created or updated
+    if (evt.type == "user.created") {
+      await prisma.user.upsert({
+        where: { clerkId: evt.data.id },
+        create: {
+          clerkId: evt.data.id,
+          email: evt.data.email_addresses[0].email_address,
+          imageUrl: evt.data.image_url,
+        },
+        update: {
+          email: evt.data.email_addresses[0].email_address,
+          imageUrl: evt.data.image_url,
+        },
+      });
+    }
+
+    // User deleted
+    if (evt.type == "user.deleted") {
+      await prisma.user.delete({
+        where: {
+          clerkId: evt.data.id,
+        },
+      });
+    }
+
+    res.json({ received: true });
+  },
+);
+
+app.use(express.json());
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
