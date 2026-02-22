bf5377cb4d0e183254c420c474dc6c9d4900706e
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-02-22 17:59:09 +0900

    feat: setup clerk and added middleware globally

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { clerkMiddleware } = require("@clerk/express");
+
+require("dotenv").config();
 
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
   }),
 );
-
-require("dotenv").config();
+app.use(clerkMiddleware());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
