6d3324eb2e27147b8ccbdd4531854adbac923fa1
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-03-08 22:00:54 +0900

    feat: view PDF of uploaded resume

--- server/routes/resumes.ts
+++ server/routes/resumes.ts
@@ -1,12 +1,46 @@
 import express, { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { r2 } from "../lib/r2";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { upload } from "../lib/multer";
 import { requireAuth } from "@clerk/express";
 import { v4 as uuidv4 } from "uuid";
 
 const resumeRouter = express.Router();
+
+resumeRouter.get("/", requireAuth(), async (req: Request, res: Response) => {
+  const { userId } = req.auth;
+
+  try {
+    const resume = await prisma.resume.findUnique({
+      where: {
+        userId: userId,
+      },
+      select: {
+        key: true,
+      },
+    });
+
+    if (!resume) {
+      res.status(404).json({ message: "Resume not found" });
+      return;
+    }
+
+    const url = await getSignedUrl(
+      r2,
+      new GetObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME!,
+        Key: resume.key,
+      }),
+      { expiresIn: 3600 },
+    );
+
+    res.json({ url });
+  } catch {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 resumeRouter.post(
   "/upload",
