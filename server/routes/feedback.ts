46d625f0d998e49c73412392244619b10a221049
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-03-11 15:20:28 +0900

    feat: add route to return openAPI query response

--- server/routes/feedback.ts
+++ server/routes/feedback.ts
@@ -1,5 +1,48 @@
 import express, { Request, Response } from "express";
+import { requireAuth } from "@clerk/express";
+import { logger } from "../lib/monitoring/logger";
+import {
+  getApplicationInfo,
+  getResumeText,
+  getTailoring,
+} from "../lib/openai/openai";
 
-const feedbackRouter = express.Router;
+// TODO: Implement audit log
+import logAudit from "../lib/monitoring/audit";
+
+const feedbackRouter = express.Router();
+
+feedbackRouter.post("/", requireAuth(), async (req: Request, res: Response) => {
+  const { userId } = req.auth;
+  const { applicationId } = req.body;
+
+  if (!userId) {
+    logger.warn("Unauthorised access attempt", { endpoint: "/feedback" });
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const application: string[] | null = await getApplicationInfo(
+      applicationId,
+      userId,
+    );
+    if (!application) {
+      logger.warn("Application does not exist", { applicationId, userId });
+      return res.status(404).json({ message: "Application not found" });
+    }
+
+    const resumeText: string | null = await getResumeText(userId);
+    if (!resumeText) {
+      logger.warn("Resume does not exist", { userId });
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    const feedback = await getTailoring(application, resumeText);
+    return res.status(200).json({ feedback });
+  } catch (error) {
+    logger.error("Failed to carry out OpenAPI request", { userId, error });
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export { feedbackRouter };
