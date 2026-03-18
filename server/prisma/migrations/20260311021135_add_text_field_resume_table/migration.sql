bca9040acfe684ea4147f82f7ee5c8dae511aebf
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-03-18 12:21:02 +0900

    fix: removed unnecessary conditional in migration sql

--- server/prisma/migrations/20260311021135_add_text_field_resume_table/migration.sql
+++ server/prisma/migrations/20260311021135_add_text_field_resume_table/migration.sql
@@ -1,10 +1,2 @@
 -- AlterTable
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM information_schema.columns 
-    WHERE table_name = 'Resume' AND column_name = 'text'
-  ) THEN
-    ALTER TABLE "Resume" ADD COLUMN "text" TEXT NOT NULL DEFAULT '';
-  END IF;
-END $$;
+ALTER TABLE "Resume" ADD COLUMN "text" TEXT NOT NULL DEFAULT '';
